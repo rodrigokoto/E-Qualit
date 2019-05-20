38603c65af8b9b3f5fd4545367839ea6f988013d
Paulo.Calligaris <Paulo.Calligaris@DEV-260917>
2019-05-20 10:55:15 -0300

    ISOTEC-16

--- ApplicationService/Servico/DocUsuarioVerificaAprovaAppServico.cs
+++ ApplicationService/Servico/DocUsuarioVerificaAprovaAppServico.cs
@@ -19,37 +19,51 @@ namespace ApplicationService.Servico
         {
             var listaDoBanco = _docUsuarioVerificaAprovaRepositorio.Get(x => x.IdDocumento == idDocumento).ToList();
 
-            var entradaFiltrado = lista.Select(x => new { x.IdDocUsuarioVerificaAprova, x.IdUsuario });
+            //var entradaFiltrado = lista.Select(x => new { x.IdDocUsuarioVerificaAprova, x.IdUsuario });
 
-            var bancoFiltrado = listaDoBanco.Select(x => new { x.IdDocUsuarioVerificaAprova, x.IdUsuario }).ToList();
+            //var bancoFiltrado = listaDoBanco.Select(x => new { x.IdDocUsuarioVerificaAprova, x.IdUsuario }).ToList();
 
-            // O q veio da tela q eu nao tenho no banco
-            var paraIncluir = entradaFiltrado.Except(bancoFiltrado);
+            //// O q veio da tela q eu nao tenho no banco
+            //var paraIncluir = entradaFiltrado.Except(bancoFiltrado);
 
-            // O q nao veio da tela mas eu tenho no banco
-            var paraDeletar = bancoFiltrado.Except(entradaFiltrado);
+            //// O q nao veio da tela mas eu tenho no banco
+            //var paraDeletar = bancoFiltrado.Except(entradaFiltrado);
 
-            List<DocUsuarioVerificaAprova> listaDel = new List<DocUsuarioVerificaAprova>();
+            //List<DocUsuarioVerificaAprova> listaDel = new List<DocUsuarioVerificaAprova>();
 
-            foreach (var del in paraDeletar)
-            {
-                listaDel.Add(listaDoBanco.Where(x => x.IdUsuario == del.IdUsuario
-                                && x.IdDocUsuarioVerificaAprova == del.IdDocUsuarioVerificaAprova).FirstOrDefault());
-            }
+            //foreach (var del in paraDeletar)
+            //{
+            //    listaDel.Add(listaDoBanco.Where(x => x.IdUsuario == del.IdUsuario
+            //                    && x.IdDocUsuarioVerificaAprova == del.IdDocUsuarioVerificaAprova).FirstOrDefault());
+            //}
 
-            foreach (var usDeletar in listaDel)
+            //foreach (var usDeletar in listaDel)
+            //{
+            //    _docUsuarioVerificaAprovaRepositorio.Remove(usDeletar);
+            //    lista.Remove(usDeletar);
+            //}
+
+            foreach (var usDeletar in listaDoBanco)
             {
                 _docUsuarioVerificaAprovaRepositorio.Remove(usDeletar);
                 lista.Remove(usDeletar);
             }
 
+            //[aquii]
             List<DocUsuarioVerificaAprova> listaIncluir = new List<DocUsuarioVerificaAprova>();
 
-            foreach (var usIncluir in paraIncluir)
+            //foreach (var usIncluir in paraIncluir)
+            //{
+            //    _docUsuarioVerificaAprovaRepositorio.Add(lista.Where(x => usIncluir.IdUsuario == x.IdUsuario
+            //                                            && x.IdDocUsuarioVerificaAprova == usIncluir.IdDocUsuarioVerificaAprova).FirstOrDefault());
+            //}
+
+            foreach (var usIncluir in lista)
             {
                 _docUsuarioVerificaAprovaRepositorio.Add(lista.Where(x => usIncluir.IdUsuario == x.IdUsuario
                                                         && x.IdDocUsuarioVerificaAprova == usIncluir.IdDocUsuarioVerificaAprova).FirstOrDefault());
             }
+
         }
 
         public void AtualizarParaEstadoInicialDoDocumento(List<DocUsuarioVerificaAprova> lista)
