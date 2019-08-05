f32a616e4ed312a1f21ca4a55742d7f9b6bfdc3b
Rodrigo Santana Pina <Rodrigo.Pina@itssolucoes.com.br>
2019-08-05 14:28:23 -0300

    Realizada a correção da tabela de não confomirdade para que seja localizado o cliente com usuário perfil Administrador

--- Dominio/Entidade/ArquivoNaoConformidadeAnexo.cs
+++ Dominio/Entidade/ArquivoNaoConformidadeAnexo.cs
@@ -10,7 +10,7 @@
         public int ApagarAnexo { get; set; } = 0;
 
         #region Relacionamento
-        public virtual RegistroConformidade NaoConformidade { get; set; }
+        public virtual RegistroAcaoImediata NaoConformidade { get; set; }
         public virtual Anexo Anexo { get; set; }
 
         #endregion
