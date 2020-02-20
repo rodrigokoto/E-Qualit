1cd67c659ed893632c15b299ec4970389dc2a6b6
Rodrigo Santana <rodrigo.santana@g2it.com.br>
2020-02-20 10:42:22 -0300

    Realizada a criação do enum de funcionalidades

---
+++ ApplicationService/Enum/FuncoesIndicadores.cs
@@ -0,0 +1,14 @@
+﻿namespace ApplicationService.Enum
+{
+    public enum FuncoesIndicadores
+    {
+        RealizarMedicao = 27,
+        RealizarAnalise = 28,
+        AnaliseDaCausa = 29,
+        DefinirAcao = 30,
+        VerificacaoDaEficacia = 31,
+        Destravar = 32,
+        AlterarMeta = 33,
+        Editar = 34,
+    }
+}
