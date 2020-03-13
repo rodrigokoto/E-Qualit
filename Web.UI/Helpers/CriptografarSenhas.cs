2c167b6c865eb81905c6c3ee40c9c84f0dac9e5c
Rodrigo Santana <rodrigo.santana@g2it.com.br>
2020-03-13 16:17:07 -0300

    Adicionada a Criptografia

---
+++ Web.UI/Helpers/CriptografarSenhas.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Web.UI.Helpers
+{
+    public class CriptografarSenhas
+    {
+        private static string _chave;
+
+
+        public CriptografarSenhas()
+        {
+        }
+
+        public static string Chave
+        {
+            set
+            {
+                _chave = value;
+            }
+        }
+
+        public static string CriptografaSenha(string senhaCripto)
+        {
+            try
+            {
+                return CriptografaSenha(senhaCripto, _chave);
+            }
+            catch (Exception ex)
+            {
+                return "String errada. " + ex.Message;
+            }
+
+        }
+
+        public static string DescriptografaSenha(string senhaDescripto)
+        {
+            try
+            {
+                return DescriptografaSenha(senhaDescripto, _chave);
+            }
+            catch (Exception ex)
+            {
+                return "Wrong Input. " + ex.Message;
+            }
+        }
+
+        public static string CriptografaSenha(string senhaCripto, string chave)
+        {
+            try
+            {
+                TripleDESCryptoServiceProvider objcriptografaSenha = new TripleDESCryptoServiceProvider();
+                MD5CryptoServiceProvider objcriptoMd5 = new MD5CryptoServiceProvider();
+
+                byte[] byteHash, byteBuff;
+                string strTempKey = chave;
+
+                byteHash = objcriptoMd5.ComputeHash(ASCIIEncoding.ASCII.GetBytes(strTempKey));
+                objcriptoMd5 = null;
+                objcriptografaSenha.Key = byteHash;
+                objcriptografaSenha.Mode = CipherMode.ECB;
+
+                byteBuff = ASCIIEncoding.ASCII.GetBytes(senhaCripto);
+                return Convert.ToBase64String(objcriptografaSenha.CreateEncryptor().TransformFinalBlock(byteBuff, 0, byteBuff.Length));
+            }
+            catch (Exception ex)
+            {
+                return "Digite os valores Corretamente." + ex.Message;
+            }
+        }
+
+
+        public static string DescriptografaSenha(string strCriptografada, string chave)
+        {
+            try
+            {
+                TripleDESCryptoServiceProvider objdescriptografaSenha = new TripleDESCryptoServiceProvider();
+                MD5CryptoServiceProvider objcriptoMd5 = new MD5CryptoServiceProvider();
+
+                byte[] byteHash, byteBuff;
+                string strTempKey = chave;
+
+                byteHash = objcriptoMd5.ComputeHash(ASCIIEncoding.ASCII.GetBytes(strTempKey));
+                objcriptoMd5 = null;
+                objdescriptografaSenha.Key = byteHash;
+                objdescriptografaSenha.Mode = CipherMode.ECB;
+
+                byteBuff = Convert.FromBase64String(strCriptografada);
+                string strDecrypted = ASCIIEncoding.ASCII.GetString(objdescriptografaSenha.CreateDecryptor().TransformFinalBlock(byteBuff, 0, byteBuff.Length));
+                objdescriptografaSenha = null;
+
+                return strDecrypted;
+            }
+            catch (Exception ex)
+            {
+                return "Digite os valores Corretamente." + ex.Message;
+            }
+        }
+
+        public bool compararStrings(string num01, string num02)
+        {
+            bool stringValor;
+            if (num01.Equals(num02))
+            {
+                stringValor = true;
+            }
+            else
+            {
+                stringValor = false;
+            }
+            return stringValor;
+        }
+
+    }
+}
