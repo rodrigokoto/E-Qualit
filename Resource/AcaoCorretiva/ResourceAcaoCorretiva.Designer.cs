e0a5b5bf427b746b312535eed5d8fbc91dae7be6
Rodrigo Santana <rodrigo.santana@g2it.com.br>
2020-10-29 10:19:52 -0300

    Realizada a adição da regra de negócio para anular uma ação corretiva

--- Resource/AcaoCorretiva/ResourceAcaoCorretiva.Designer.cs
+++ Resource/AcaoCorretiva/ResourceAcaoCorretiva.Designer.cs
@@ -19,7 +19,7 @@ namespace Traducao.AcaoCorretiva {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class ResourceAcaoCorretiva {
@@ -417,6 +417,15 @@ namespace Traducao.AcaoCorretiva {
         public static string AC_lbl_Status_Acao_imediata {
             get {
                 return ResourceManager.GetString("AC_lbl_Status_Acao_imediata", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Anulada.
+        /// </summary>
+        public static string AC_lbl_Status_Anulada {
+            get {
+                return ResourceManager.GetString("AC_lbl_Status_Anulada", resourceCulture);
             }
         }
         
